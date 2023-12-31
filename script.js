59dc2fe2c8eaa6dde72bab154cf89d496c27e6e6
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2023-12-31 12:25:37 -0500

    Got function to display text using inner text

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 function generateMovieQuote() {
   const  movieQuotes = ["I'm going to make him an offer he can't refuse.","Houston, we have a problem.","I feel the need — the need for speed!"];
 
-  const randomQuotes = movieQuotes[(Math.floor(Math.random() * movieQuotes.length))];
-  console.log(randomQuotes)
+  let randomQuotes = movieQuotes[(Math.floor(Math.random() * movieQuotes.length))];
+
+  const paragraphEl = document.querySelector('.display-quote');
+
+  paragraphEl.innerText = randomQuotes
+
 }
 
-generateMovieQuote()
+
 
