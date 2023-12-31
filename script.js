9940920393c9419a22b325f80f881f9bede8d781
dbrah-design <121207906+dbrah-design@users.noreply.github.com>
2023-12-31 16:49:40 -0500

    Added another quote to array

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 function generateMovieQuote() {
-  const  movieQuotes = ["I'm going to make him an offer he can't refuse.","Houston, we have a problem.","I feel the need — the need for speed!"];
+  const  movieQuotes = ["I'm going to make him an offer he can't refuse."
+  ,"Houston, we have a problem."
+  ,"I feel the need — the need for speed!",
+  "May the Force be with you."];
 
   let randomQuotes = movieQuotes[(Math.floor(Math.random() * movieQuotes.length))];
 
